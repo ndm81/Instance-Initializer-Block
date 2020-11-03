7c6805d02eb1b8d0c4df39b0d71540cd7731a459
Nadeem012 <55021916+ndm81@users.noreply.github.com>
2020-11-03 22:04:51 +0530

    Add files via upload

---
+++ ins.java
@@ -0,0 +1,20 @@
+class Car{
+	int speed;  // Instance Variable
+
+	Car() // Constructor of the Car Class
+	{
+		System.out.println("Speed of the car is "+ speed);
+		}
+			{
+				speed=120; 
+			}
+		
+
+		    public static void main(String[] args) {
+			Car c=new Car(); // object of the Car
+
+		}
+	}
+
+
+  
